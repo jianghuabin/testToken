7d5a83add3c6aa4815ef55744458ed7270762098
qaqzzz <2964833024@qq.com>
2017-01-19 10:53:27 +0800

    token修改
    token 验证前端提示，以及根据需求提供数据。

---
+++ src/com/lsd/testToken/exception/TokenException.java
@@ -0,0 +1,30 @@
+package com.lsd.testToken.exception;
+
+public class TokenException extends RuntimeException {
+
+	public TokenException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TokenException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TokenException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TokenException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TokenException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
